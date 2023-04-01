cea77b2fcbf198a9d1fa3f31f1c2807238207230
Ahmad Dzulfikar Ubaidillah <dzulfprojects@gmail.com>
2023-04-01 16:43:31 +0700

    write file

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body');
+    who_btn = document.querySelector('.who-btn');
+    sec_1 = document.querySelector('.sec-1');
+    
+    
+who_btn.addEventListener("click", ()=>{
+    sec_1.classList.add("active");
+})
